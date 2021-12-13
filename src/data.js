9815d5f53fce854e216e970f6ac00b943726dee6
taylor d <taylor.delph@eyecuelab.com>
2021-12-13 15:05:53 -0800

    add delete function to fake data

--- src/data.js
+++ src/data.js
@@ -34,3 +34,15 @@ let invoices = [
 export function getInvoices() {
   return invoices;
 }
+
+export function getInvoice(number) {
+  return invoices.find(
+    invoice => invoice.number === number
+  );
+}
+
+export function deleteInvoice(number) {
+  invoices = invoices.filter(
+    invoice => invoice.number !== number
+  );
+}
