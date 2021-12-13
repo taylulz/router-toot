a37fd37869814cf17198e72e70e1b6dcb2e04c31
taylor d <taylor.delph@eyecuelab.com>
2021-12-13 13:13:20 -0800

    optionally persist query string

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,8 +1,8 @@
 import {
-  NavLink,
   Outlet,
   useSearchParams
 } from "react-router-dom";
+import QueryNavLink from "../QueryNavLink";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
@@ -36,7 +36,7 @@ export default function Invoices() {
             return name.startsWith(filter.toLowerCase());
           })
           .map(invoice => (
-            <NavLink
+            <QueryNavLink
               style={({ isActive }) => ({
                 display: "block",
                 margin: "1rem 0",
@@ -46,7 +46,7 @@ export default function Invoices() {
               key={invoice.number}
             >
               {invoice.name}
-            </NavLink>
+            </QueryNavLink>
           ))}
       </nav>
       <Outlet />
